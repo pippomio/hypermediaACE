f23e3b5cf33abbf36c72eec10bdadb5d05433371
pippomio <edoardo.90@alice.it>
2012-06-27 14:55:52 +0200

    creata Cpage da mac mini, log out di pippomio

---
+++ JavistiProgectACE/src/it/polimi/aip/javisti/controller/cpage/InsallController.java
@@ -0,0 +1,12 @@
+package it.polimi.aip.javisti.controller.cpage;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+public class InsallController extends Controller {
+
+    @Override
+    public Navigation run() throws Exception {
+        return forward("insall.jsp");
+    }
+}
